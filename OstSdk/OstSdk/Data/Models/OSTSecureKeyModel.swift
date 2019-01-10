52f79edfd9b80123dbe665d9b7691199a12dd2bb
aniket ayachit <aniket@pacewisdom.com>
2019-01-10 18:06:58 +0530

    protocol added for srecureKey

--- OstSdk/OstSdk/Data/Models/OSTSecureKeyModel.swift
+++ OstSdk/OstSdk/Data/Models/OSTSecureKeyModel.swift
@@ -8,7 +8,10 @@
 
 import Foundation
 
-
 protocol OSTSecureKeyModel: class {
-
+    func get(_ id: String) throws -> OSTSecureKey?
+    
+    func save(_ secureKeyData: [String : Any], success: ((OSTSecureKey?) -> Void)?, failure: ((Error) -> Void)?)
+    
+    func delete(_ id: String, success: ((Bool)->Void)?)
 }
