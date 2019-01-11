32d5b65f7a2cc4804b43ca5d22dbf0bd0b995089
aniket ayachit <aniket@pacewisdom.com>
2019-01-11 15:07:19 +0530

    extension for QR code generation and read added.

---
+++ OstSdk/OstSdk/Data/Utils/Extension+OSTQRCode.swift
@@ -0,0 +1,54 @@
+//
+//  Extension + OSTQRCode.swift
+//  OstSdk
+//
+//  Created by aniket ayachit on 11/01/19.
+//  Copyright © 2019 aniket ayachit. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    public var qrCode: CIImage?  {
+        let data = self.data(using: .isoLatin1, allowLossyConversion: false)
+        guard let filter = CIFilter(name: "CIQRCodeGenerator") else {
+            return nil
+        }
+        
+        filter.setValue(data, forKey: "inputMessage")
+        filter.setValue("M", forKey: "inputCorrectionLevel")
+        
+        return filter.outputImage
+    }
+}
+
+extension UIImage {
+    public var readQRCode: [String]? {
+        guard let ciImage = self.ciImage else {
+            return nil
+        }
+        let detector = CIDetector(ofType: CIDetectorTypeQRCode,
+                                  context: nil,
+                                  options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
+        
+        let features = detector?.features(in: ciImage) ?? []
+        
+        return features.compactMap { feature in
+            return (feature as? CIQRCodeFeature)?.messageString
+        }
+    }
+}
+
+extension CIImage {
+    public var readQRCode: [String]? {
+        let detector = CIDetector(ofType: CIDetectorTypeQRCode,
+                                  context: nil,
+                                  options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
+        
+        let features = detector?.features(in: self) ?? []
+        
+        return features.compactMap { feature in
+            return (feature as? CIQRCodeFeature)?.messageString
+        }
+    }
+}
