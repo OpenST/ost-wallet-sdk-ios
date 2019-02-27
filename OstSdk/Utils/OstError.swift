fb68e4d03a0ae8518bac9da4162a3440f161cc46
aniket ayachit <aniket@pacewisdom.com>
2019-02-27 19:12:56 +0530

    pin validate fallback added.

--- OstSdk/Utils/OstError.swift
+++ OstSdk/Utils/OstError.swift
@@ -101,6 +101,7 @@ public enum OstErrorText: String {
     case signerAddressNotFound = "Signer address not found."
     case invalidQRCode = "Invalid QR Code"
     case maxUserValidatedCountReached = "Exceeded pin retry limit."
+    case recoveryPinNotFoundInKeyManager = "recovery pin not found in key manager."
     //API-Errors
     case invalidApiEndPoint = "Invalid Api Endpoint"
     case apiSignatureGenerationFailed = "Api Signature generation failed."
