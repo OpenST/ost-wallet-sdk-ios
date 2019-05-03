c9d60fc7044cb50a75bd653c0ccbaa4742fcc3b2
Aniket <aniket@ost.com>
2019-05-03 15:01:37 +0530

    AuthorizeDeviceViewController added.

---
+++ demo-app/TestDemoApp/SettingOptions/AuthorizeDeviceViewController.swift
@@ -0,0 +1,117 @@
+//
+//  AuthorizeDeviceViewController.swift
+//  TestDemoApp
+//
+//  Created by aniket ayachit on 03/05/19.
+//  Copyright © 2019 aniket ayachit. All rights reserved.
+//
+
+import UIKit
+
+class AuthorizeDeviceViewController: BaseSettingOptionsViewController {
+
+    //MAKR: - Components
+    let authorizeViaQRButton: UIButton = {
+        let view = OstUIKit.primaryButton();
+        view.setTitle("Authorize with QR", for: .normal);
+        return view;
+    }()
+    let authorizeViaMnemonicsButton: UIButton = {
+        let view = OstUIKit.secondaryButton()
+        view.setTitle("Authorize with Mnemonics", for: .normal);
+        return view;
+    }()
+    let recoverWithPinButton: UIButton = {
+        let view = OstUIKit.secondaryButton()
+        view.setTitle("Recover with PIN", for: .normal);
+        return view;
+    }()
+    
+    let infoTextForRecovery: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Don’t have a secondary device or mnemonics?"
+        return label
+    }()
+    
+    //MARK: Themer
+    var infoTextForRecoveryThemer = OstTheme.leadLabel
+    
+    //MAKR: - View LC
+    override func getNavBarTitle() -> String {
+        return "Authorize Device"
+    }
+    
+    override func getLeadLabelText() -> String {
+        return "We notice that this is not an authorized device. Please recover your wallet from the following options."
+    }
+    
+    //MAKR: - Create Views
+    
+    override func addSubviews() {
+        super.addSubviews()
+        svContentView.addSubview(authorizeViaQRButton)
+        svContentView.addSubview(authorizeViaMnemonicsButton)
+        svContentView.addSubview(recoverWithPinButton)
+        svContentView.addSubview(infoTextForRecovery)
+        
+        infoTextForRecoveryThemer.apply(infoTextForRecovery)
+       
+        weak var weakSelf = self
+        authorizeViaQRButton.addTarget(weakSelf, action: #selector(weakSelf!.authorizeViaQRButtonTapped(_:)), for: .touchUpInside)
+        authorizeViaMnemonicsButton.addTarget(weakSelf, action: #selector(weakSelf!.authorizeViaMnemonicsButtonTapped(_:)), for: .touchUpInside)
+        recoverWithPinButton.addTarget(weakSelf, action: #selector(weakSelf!.recoverWithPinButton(_:)), for: .touchUpInside)
+    }
+    
+    //MARK: - Apply Constraints
+    
+    override func addLayoutConstraints() {
+        super.addLayoutConstraints()
+        addAuthorizeViaQRButtonConstraints()
+        addAuthorizeViaMnemonicsButtonConstraints()
+        addInfoTextForRecoveryConstraints()
+        addRecoverWithPinButtonConstraints()
+    }
+    
+    func addAuthorizeViaQRButtonConstraints() {
+        authorizeViaQRButton.placeBelow(toItem: leadLabel)
+        authorizeViaQRButton.setW375Width(width: 315)
+        authorizeViaQRButton.setAspectRatio(width: 315, height: 50)
+        authorizeViaQRButton.centerXAlignWithParent()
+    }
+    
+    func addAuthorizeViaMnemonicsButtonConstraints() {
+        authorizeViaMnemonicsButton.placeBelow(toItem: authorizeViaQRButton)
+        authorizeViaMnemonicsButton.setW375Width(width: 315)
+        authorizeViaMnemonicsButton.setAspectRatio(width: 315, height: 50)
+        authorizeViaMnemonicsButton.centerXAlignWithParent()
+    }
+    
+    func addInfoTextForRecoveryConstraints() {
+        infoTextForRecovery.placeBelow(toItem: authorizeViaMnemonicsButton, constant: 200)
+        infoTextForRecovery.applyBlockElementConstraints()
+    }
+    
+    func addRecoverWithPinButtonConstraints() {
+        recoverWithPinButton.placeBelow(toItem: infoTextForRecovery)
+        recoverWithPinButton.applyBlockElementConstraints()
+        recoverWithPinButton.bottomAlignWithParent(multiplier: 1, constant: 38)
+    }
+    
+    //MARK: - Button Actions
+    
+    @objc func authorizeViaQRButtonTapped(_ sender: Any?) {
+        let authorizeViaQRVC = QRScannerViewController()
+        authorizeViaQRVC.pushWith(self, animated: true)
+    }
+    
+    @objc func authorizeViaMnemonicsButtonTapped(_ sender: Any?) {
+        let authorizeViaMnemonicsVC = AuthorizeDeviceViaMnemonicsViewController()
+        authorizeViaMnemonicsVC.pushWith(self, animated: true)
+    }
+    
+    @objc func recoverWithPinButton(_ sender: Any?) {
+        
+    }
+}
