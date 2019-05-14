a099ac4908fa5d9b7a954f8657c40db30682d64c
Aniket <aniket@ost.com>
2019-05-15 01:19:03 +0530

    notification view added.

--- demo-app/TestDemoApp/WorkflowUI/Views/Notification/OstNotification.swift
+++ demo-app/TestDemoApp/WorkflowUI/Views/Notification/OstNotification.swift
@@ -11,6 +11,134 @@
 import Foundation
 import UIKit
 
-class OstNotification: UIView {
+class OstNotification: OstBaseView {
     
+    //MARK: - Components
+    let containerView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .clear
+        view.layer.cornerRadius = 6
+        view.clipsToBounds = true
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var titleLabel: UILabel = {
+       let label = OstUIKit.leadLabel()
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.text = "Aniket"
+        return label
+    }()
+    
+    var imageView: UIImageView = {
+        let imageView = UIImageView(image: nil)
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    //MARK: - Add Subview
+    override func createViews() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.textAlignment = .left
+        titleLabel.lineBreakMode = .byTruncatingTail
+        containerView.backgroundColor = getContainerBackgorundColor()
+        imageView.image = getImage()
+        addShadow()
+        
+        containerView.addSubview(imageView)
+        containerView.addSubview(titleLabel)
+        addSubview(containerView)
+    }
+    
+    func addShadow() {
+        layer.shadowColor = UIColor.lightGray.cgColor
+        layer.shadowOpacity = 1
+        layer.shadowOffset = CGSize(width: 0, height: 3)
+        layer.shadowRadius = 6
+    }
+    
+    //MAKR: - Components Settings
+    func getContainerBackgorundColor() -> UIColor {
+        return .lightGray
+    }
+    
+    func getImage() -> UIImage? {
+        return nil
+    }
+
+    //MARK: - Add Constraints
+    
+    override func applyConstraints() {
+        applyContainerViewConstraints()
+        applyTitleLabelConstraints()
+        applyImageViewConstraints()
+    }
+    
+    func applySelfConstraints() {
+        self.topAlignWithParent()
+        self.applyBlockElementConstraints(horizontalMargin: 0)
+        self.bottomAlign(toItem: containerView)
+    }
+    
+    func applyContainerViewConstraints() {
+        topAnchorConstraint = containerView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: -100)
+        topAnchorConstraint!.isActive = true
+        containerView.leftAlignWithParent(multiplier: 1, constant: 10)
+        containerView.rightAlignWithParent(multiplier: 1, constant: -10)
+        containerView.bottomAlign(toItem: titleLabel, constant: 10)
+    }
+    
+    func applyTitleLabelConstraints() {
+        titleLabel.topAlignWithParent(multiplier: 1, constant: 10)
+        titleLabel.leftWithRightAlign(toItem: imageView, constant:8)
+        titleLabel.rightAlignWithParent(multiplier: 1, constant: -8)
+    }
+    
+    func applyImageViewConstraints() {
+        imageView.topAlignWithParent(multiplier: 1, constant: 10)
+        imageView.leftAlignWithParent(multiplier: 1, constant: 10)
+        imageView.setFixedWidth(constant: 24)
+        imageView.setFixedHeight(multiplier: 1, constant: 24)
+    }
+    
+    //MARK: - Variables
+    var topAnchorConstraint: NSLayoutConstraint? = nil
+    
+    var title: String! {
+        didSet {
+            self.titleLabel.text = title
+        }
+    }
+    
+    func show() {
+        applySelfConstraints()
+       
+        self.layoutIfNeeded()
+        
+        topAnchorConstraint?.constant = 10
+        self.containerView.alpha = 0.3
+        UIView.animate(withDuration: 0.3, animations: {
+            self.containerView.alpha = 1
+            self.layoutIfNeeded()
+        }) { (isComplete) in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2, execute: {[weak self] in
+                self?.hide()
+            })
+        }
+    }
+    
+    func hide() {
+        topAnchorConstraint?.constant = -100
+        self.containerView.alpha = 1.0
+        UIView.animate(withDuration: 0.3, animations: {
+            self.containerView.alpha = 0.0
+            self.layoutIfNeeded()
+        }) {[weak self] (isComplete) in
+           self?.removeFromSuperview()
+        }
+        
+    }
 }
