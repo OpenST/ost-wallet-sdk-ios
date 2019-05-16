b89da808008204a02769eff781957f309400728c
Aniket <aniket@ost.com>
2019-05-16 16:37:43 +0530

    Tag for device added.

--- demo-app/TestDemoApp/Cell/TabelViewCell/DeviceTableViewCell.swift
+++ demo-app/TestDemoApp/Cell/TabelViewCell/DeviceTableViewCell.swift
@@ -25,14 +25,40 @@ class DeviceTableViewCell: UsersTableViewCell {
         return view
     }()
     
+    var tagLabel: UILabel = {
+        let label = InsetLabel()
+        label.font = OstFontProvider().get(size: 11)
+        label.textColor = UIColor.color(52, 68, 91)
+        label.backgroundColor = UIColor.color(239, 249, 250)
+        label.layer.cornerRadius = 5
+        label.textAlignment = .center
+        label.clipsToBounds = true
+        label.padding = 5
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        return label
+    }()
+    
     var sendButtonHeightConstraint: NSLayoutConstraint? = nil
 
     //MARK: - Add Subview
+    override func createViews() {
+        super.createViews()
+        
+        self.detailsContainerView?.addSubview(tagLabel)
+    }
+    
     override func createInternalView() {
         self.circularView!.addSubview(overlayImage)
     }
     
     //MARK: - Apply Constraints
+    override func applyConstraints() {
+        super.applyConstraints()
+        
+        applyTagLabelConstraints()
+    }
+    
     override func applyInitialLetterConstraints() {
         self.overlayImage.centerYAnchor.constraint(equalTo: self.circularView!.centerYAnchor).isActive = true
         self.overlayImage.centerXAnchor.constraint(equalTo: self.circularView!.centerXAnchor).isActive = true
@@ -56,6 +82,21 @@ class DeviceTableViewCell: UsersTableViewCell {
         sendButtonHeightConstraint?.isActive = true
     }
     
+    override func applyTitleLabelConstraitns() {
+        guard let parent = self.titleLabel?.superview else {return}
+        self.titleLabel?.translatesAutoresizingMaskIntoConstraints = false
+        self.titleLabel?.topAnchor.constraint(equalTo: parent.topAnchor).isActive = true
+        self.titleLabel?.leftAnchor.constraint(equalTo: parent.leftAnchor).isActive = true
+    }
+    
+    func applyTagLabelConstraints() {
+        guard let parent = self.titleLabel?.superview else {return}
+        self.tagLabel.topAnchor.constraint(equalTo: parent.topAnchor).isActive = true
+        self.tagLabel.leftAnchor.constraint(equalTo: titleLabel!.rightAnchor, constant: 10).isActive = true
+        self.tagLabel.centerYAnchor.constraint(equalTo: titleLabel!.centerYAnchor).isActive = true
+    }
+    
+    //MARK: - Action
     override func sendButtonTapped(_ sender: Any?) {
         sendButtonAction?(self.deviceDetails)
     }
@@ -69,22 +110,22 @@ class DeviceTableViewCell: UsersTableViewCell {
     var deviceDetails: [String: Any]! {
         didSet {
             self.balanceLabel?.text = deviceDetails["address"] as? String ?? ""
-            let titleText = getButtonTitleForDeviceState()
-            if titleText.isEmpty {
+            let buttonTitle = getButtonTitleForDeviceState()
+            if buttonTitle.isEmpty {
                 sendButtonHeightConstraint?.constant = 0.0
                 sendButton?.isHidden = true
             }else {
                 sendButtonHeightConstraint?.constant = 30.0
                 sendButton?.isHidden = false
-                sendButton?.setTitle(titleText, for: .normal)
+                sendButton?.setTitle(buttonTitle, for: .normal)
                 
                 let deviceStatus = deviceDetails["status"] as! String
                 
                 let currentUserDevice = CurrentUserModel.getInstance.currentDevice!
-                
                 if address.caseInsensitiveCompare(currentUserDevice.address!) == .orderedSame {
                     sendButton?.isEnabled = false
                 }
+                    
                 else if (!currentUserDevice.isStatusRegistered
                     && status.caseInsensitiveCompare(ManageDeviceViewController.DeviceStatus.registered.rawValue) == .orderedSame) {
                     
@@ -96,6 +137,14 @@ class DeviceTableViewCell: UsersTableViewCell {
                 else {
                     sendButton?.isEnabled = true
                 }
+            }
+            
+            let tagText = getTagText()
+            if tagText.isEmpty {
+                tagLabel.isHidden = true
+            }else {
+                tagLabel.isHidden = false
+                tagLabel.text = tagText
             }
         }
     }
@@ -131,6 +180,23 @@ class DeviceTableViewCell: UsersTableViewCell {
             return ""
         }
         return ""
+    }
+    
+    func getTagText() -> String {
+        var tagText: String = ""
+        
+        if let deviceStatus = deviceDetails["status"] as? String {
+            if deviceStatus.caseInsensitiveCompare(ManageDeviceViewController.DeviceStatus.recovering.rawValue) == .orderedSame {
+                tagText = "🕘 Recovery in progress"
+            }
+        }
+        
+        let currentUserDevice = CurrentUserModel.getInstance.currentDevice!
+        if address.caseInsensitiveCompare(currentUserDevice.address!) == .orderedSame {
+            tagText = tagText.isEmpty ? "This Device" : tagText
+        }
+        
+        return tagText
     }
     
     //MARK: - Variables
