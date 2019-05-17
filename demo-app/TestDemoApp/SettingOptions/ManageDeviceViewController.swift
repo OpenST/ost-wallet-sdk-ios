4a6378e13c3b6fd855e2b37aa3b6efc8d74e7181
Aniket <aniket@ost.com>
2019-05-18 00:43:37 +0530

    text fix.

--- demo-app/TestDemoApp/SettingOptions/ManageDeviceViewController.swift
+++ demo-app/TestDemoApp/SettingOptions/ManageDeviceViewController.swift
@@ -29,7 +29,7 @@ class ManageDeviceViewController: BaseSettingOptionsViewController, UITableViewD
     }
     
     override func getLeadLabelText() -> String {
-        return "Here are your authorized devices"
+        return "Here are your devices"
     }
     
     //MAKR: - Components
